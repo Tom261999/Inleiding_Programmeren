f1c5bd8033e4f9775b35595c1d0b9cf83f3a2c76
Tom <43322000+Tom261999@users.noreply.github.com>
2019-02-21 22:21:07 +0100

    Opdracht1 finished

--- Opdracht1/Opdracht1/roots.cpp
+++ Opdracht1/Opdracht1/roots.cpp
@@ -8,7 +8,7 @@ double root2(double S, double x0, double epsilon)
 {
 	if ((S < 0) || (x0 <= 0) || (epsilon <= 0))
 	{
-		cout << "Invalid Input" << endl;
+		cout << "Invalid input: some or all values are negative." << endl;
 		return 0;
 	}
 	double xa = 0;
@@ -21,11 +21,29 @@ double root2(double S, double x0, double epsilon)
 	return xb;
 }
 
+double root2alt(double S, double x0, double epsilon)
+{
+	int iter = 1;
+	double xa = 0;
+	double xb = x0;
+	cout << "startwaarde: " << x0 << endl;
+	cout << "iteratie - output" << endl;
+	do
+	{
+		xa = xb;
+		xb = 0.5*(xa + S / xa);
+		cout << iter << " , " << xb << " ; " << endl;
+		iter++;
+	} while (abs(xb - xa) > epsilon);
+	cout << "(Opmerking: deze data is zo geformatteerd opdat deze makkelijk in MatLab ingelezen kon worden met de structuur van een matrix.)" << endl;
+	return xb;
+}
+
 double rootn(double S, double x0, double epsilon, int n)
 {
 	if ((((S < 0) || (x0 <= 0)) && (n % 2 == 0)) || (epsilon <= 0) || (n <= 0) || (x0 == 0))
 	{
-		cout << "Invalid Input" << endl;
+		cout << "Invalid input: one (or more) of the entered values cannot be negative." << endl;
 		return 0;
 	}
 
--- Opdracht1/Opdracht1/roots.h
+++ Opdracht1/Opdracht1/roots.h
@@ -7,5 +7,6 @@ using namespace std;
 
 double root2(double S, double x0, double epsilon);
 double rootn(double S, double x0, double epsilon, int n);
+double root2alt(double S, double x0, double epsilon);
 
 #endif
